abf04ebd44ba9af24bb6fefe09a5d3fff7d1783c
Ricardo <cadimrosalem@gmail.com>
2025-01-13 11:37:35 -0300

    declarando variáveis

--- 01-fundamentos-do-javascript/01-fundamentos-do-javascript/inicio/script.js
+++ 01-fundamentos-do-javascript/01-fundamentos-do-javascript/inicio/script.js
@@ -11,12 +11,59 @@
          let nome = "Ricardo";
          console.log("Olá, " + nome + "!")
          alert("Olá, " + nome + "!");
+        
+
+        Adição
+        let soma = 5 + 4;
+        console.log("A soma é",soma);
+        console.log(soma + 1);
+
+        Subtração
+        let subtracao = 10 - 4;
+        console.log("A subtração é",subtracao);
+
         */
 
-        // Adição
-        let soma = 5 + 3;
-        console.log("A soma é",soma)
+        /*
+        let idade;
+        idade = 25;
 
-        // Subtração
-        let subtracao = 10 - 4;
-        console.log("A subtração é",subtracao)
+        console.log(idade);
+
+        idade = 32;
+        console.log(idade);
+
+        idade = idade + 1;
+        console.log(idade);
+        */
+
+        // Declaração de variável
+        /*
+        var nome = "João";
+        console.log(nome);
+
+        console.log("Bom dia, "+nome);
+        console.log("O nome é "+nome);
+        */
+
+        var idade = 25;
+        var altura = 1.80;
+        var possuiCarro = true;
+        var nome = "João";
+
+        // Exibir os valores das variáveis
+
+        console.log("Nome: ",nome);
+        console.log("Idade: ",idade);
+        console.log("Altura: ",altura);
+        console.log("Possui carro? ",possuiCarro)
+
+        var idade = 30;
+        var altura = 1.65;
+        var possuiCarro = false;
+        var nome = "Maria";
+
+        console.log("Nome: ",nome);
+        console.log("Idade: ",idade);
+        console.log("Altura: ",altura);
+        console.log("Possui carro? ",possuiCarro);
